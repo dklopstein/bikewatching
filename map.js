a89665b1a7a47a46d5add8e73a6964d67335bceb
dklopstein <dklopstein@ucsd.edu>
2025-02-19 15:44:01 -0800

    added bike lanes to map

--- map.js
+++ map.js
@@ -11,3 +11,21 @@ const map = new mapboxgl.Map({
   minZoom: 5, // Minimum allowed zoom
   maxZoom: 18, // Maximum allowed zoom
 });
+
+map.on("load", () => {
+  map.addSource("boston_route", {
+    type: "geojson",
+    data: "https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson?...",
+  });
+
+  map.addLayer({
+    id: "bike-lanes",
+    type: "line",
+    source: "boston_route",
+    paint: {
+      "line-color": "#32D400",
+      "line-width": 4,
+      "line-opacity": 0.4,
+    },
+  });
+});
